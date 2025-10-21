307fb062aba507c2a122f7835310d645e2f2f90e
Barbara Dapper <barbaradasilvadapper@gmail.com>
2025-10-21 14:36:09 -0300

    first commit

--- Sources/CustomButtonKit/CustomButtonKit.swift
+++ Sources/CustomButtonKit/CustomButtonKit.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct CustomButton: View {
+    private let title: String
+    private let action: () -> Void
+    
+    init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+        }
+    }
+}
